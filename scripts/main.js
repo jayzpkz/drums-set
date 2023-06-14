0cff944150e304ab459098a08772bbb2ebe5d42a
Arie Fishman <57365299+jayzpkz@users.noreply.github.com>
2023-06-14 22:58:20 +0300

    added keyboard keys + new images + new sounds

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,37 @@
+// elements
 const drums = document.querySelectorAll('.drum');
 const sounds = document.querySelectorAll('.sound');
 
-const handleDrumClick = (index) => {
+// functions
+const handleDrumClick = (event, index) => {
+  // executing the sound of the selected drum or connected key
   const currentSound = sounds[index];
   currentSound.currentTime = 0;
   currentSound.play();
 };
 
+// event listeners
 drums.forEach((drum, index) => {
-  drum.addEventListener('click', () => handleDrumClick(index));
+  drum.addEventListener('click', (event) => handleDrumClick(event, index));
 });
 
+window.addEventListener('keydown', (event) => {
+  // getting the pressed key
+  const keyPressed = event.key.toLowerCase();
+  const keyMapping = {
+    w: 0,
+    a: 1,
+    s: 2,
+    d: 3,
+    h: 4,
+    j: 5,
+    k: 6,
+    l: 7
+  };
+
+  // checking if the pressed key value exists in keyMapping object
+  // if it exists, handleDrumClick is being executed.
+  if (keyMapping.hasOwnProperty(keyPressed)) {
+    handleDrumClick(event, keyMapping[keyPressed]);
+  }
+});
